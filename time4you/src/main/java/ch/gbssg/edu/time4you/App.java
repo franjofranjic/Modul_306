67632108c7687f5278e7d3913a49856ba7834363
franjofranjic <franjo.franjic@edu.gbssg.ch>
2019-11-21 19:20:42 +0100

    Maven Project

---
+++ time4you/src/main/java/ch/gbssg/edu/time4you/App.java
@@ -0,0 +1,13 @@
+package ch.gbssg.edu.time4you;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
